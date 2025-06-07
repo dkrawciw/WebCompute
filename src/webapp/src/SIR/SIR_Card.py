0f08f0124b8892a147f45a31879348bc0b7b1448
Daniel Krawciw <daniel.krawciw@gmail.com>
2025-06-06 21:31:47 -0600

    Added the plots to the main webpage to be able to simulate the curves qucikly

--- src/webapp/src/SIR/SIR_Card.py
+++ src/webapp/src/SIR/SIR_Card.py
@@ -1,5 +1,9 @@
 from dash import html, dcc, callback, Input, Output, State
 import dash_bootstrap_components as dbc
+from .SIR import SIR
+
+import pandas as pd
+import plotly.express as px
 
 def Sir_Input(label: str, id: str, placeholder:str = "", default_value: float = 0):
     return html.Div([
@@ -28,41 +32,49 @@ def SIR_Card() -> html:
 
             dbc.Button("Simulate", id="sir_basic_btn"),
 
-            html.Div(id="sir_basic_output"),
+            html.Div([], id="sir_basic_output"),
         ]),
     ])
 
-# @callback(
-#     Output("sir_basic_output", "fig"),
-#     Input("sir_basic_btn", "n_clicks"),
-#     [
-#         State("sir_basic_S_IC", "value"),
-#         State("sir_basic_I_IC", "value"),
-#         State("sir_basic_R_IC", "value"),
-#         State("sir_basic_beta", "value"),
-#         State("sir_basic_gamma", "value"),
-#         State("sir_basic_tlength", "value"),
-#     ],
-#     prevent_initial_call=True,
-# )
-# def simulate_basic_sir(sir_basic_btn, sir_basic_S_IC,
-#                        sir_basic_I_IC, sir_basic_R_IC,
-#                        sir_basic_beta, sir_basic_gamma,
-#                        sir_basic_tlength):
-#     # Setup diffeq
-#     set_sir_basic_fun = lambda t, y: SIR_basic(t, y, beta=sir_basic_beta, gamma=sir_basic_gamma)
-#     t_span = [0, sir_basic_tlength]
-#     y0 = np.array([sir_basic_S_IC, sir_basic_I_IC, sir_basic_R_IC])
+@callback(
+    Output("sir_basic_output", "children"),
+    Input("sir_basic_btn", "n_clicks"),
+    [
+        State("sir_basic_S_IC", "value"),
+        State("sir_basic_I_IC", "value"),
+        State("sir_basic_R_IC", "value"),
+        State("sir_basic_beta", "value"),
+        State("sir_basic_gamma", "value"),
+        State("sir_basic_tlength", "value"),
+    ],
+    prevent_initial_call=True,
+)
+def simulate_basic_sir(sir_basic_btn, sir_basic_S_IC,
+                       sir_basic_I_IC, sir_basic_R_IC,
+                       sir_basic_beta, sir_basic_gamma,
+                       sir_basic_tlength):
+    new_sir = SIR()
+    new_sir.beta = sir_basic_beta
+    new_sir.gamma = sir_basic_gamma
+    new_sir.t_length = sir_basic_tlength
 
-#     # Solve diffeq
-#     soln = solve_ivp( fun=set_sir_basic_fun, t_span=t_span, y0=y0 )
+    new_sir.S0 = sir_basic_S_IC
+    new_sir.I0 = sir_basic_I_IC
+    new_sir.R0 = sir_basic_R_IC
 
-#     df = pd.DataFrame(soln.y)
-#     df['t'] = soln.t
+    soln = new_sir.solve_basic_sir()
 
-#     print( df )
+    df = pd.DataFrame( soln.y.T )
+    df['t'] = soln.t.T
+    df.columns = ["S","I","R","t"]
 
-#     # Plot Solution
-#     fig = None
+    hist_plot = px.histogram(df, x="t", y=["S", "I", "R"],
+                             barmode="stack", histfunc="avg",
+                             nbins=df.shape[0])
     
-#     return fig
+    line_plot = px.line(df, x="t", y=["S","I","R"])
+
+    return [
+        dcc.Graph(figure=hist_plot),
+        dcc.Graph(figure=line_plot),
+    ]
